bb685159e4b0afea521e5dd51e3aa248048f32c8
DESKTOP-JB3GCLJ\Mr_Schyzo <catapano-marco@mail.com>
2018-06-21 02:54:03 +0200

    Adds a static utility method to create a RLStatsAPIClient instance

--- src/main/kotlin/com/mrschyzo/api/rlstats/clients/RLStatsAPIClient.kt
+++ src/main/kotlin/com/mrschyzo/api/rlstats/clients/RLStatsAPIClient.kt
@@ -1,9 +1,21 @@
 package com.mrschyzo.api.rlstats.clients
 
-import com.mrschyzo.api.rlstats.providers.RLDataProvider
-import com.mrschyzo.api.rlstats.providers.RLStatsProvider
+import com.mrschyzo.api.rlstats.providers.*
+import com.mrschyzo.api.rlstats.utils.data.*
 
 class RLStatsAPIClient (
         private val statsClient: RLStatsProvider,
         private val dataContainer: RLDataProvider
 ) : RLStatsProvider by statsClient, RLDataProvider by dataContainer
+{
+    companion object {
+        fun createFromProps(props: RetrofitServiceProperties) : RLStatsAPIClient {
+            val service = props.generateRLService()
+
+            return RLStatsAPIClient(
+                statsClient = ApiRLStatsProvider(service),
+                dataContainer = IndexedRLDataProvider(service)
+            )
+        }
+    }
+}
